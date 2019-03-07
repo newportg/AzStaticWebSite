a3c8c928cde8714ff6c39bb01cc3ce9515d11b4d
Gary Newport <gary.newport@zoomalong.co.uk>
2019-03-07 21:05:37 +0000

    Add project files.

---
+++ Function1.cs
@@ -0,0 +1,42 @@
+using System.Net;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Threading.Tasks;
+using System;
+using HeyRed.Mime;
+
+namespace AzWebsite
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            try
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK);
+                var stream = new FileStream(@"www\index.html", FileMode.Open);
+                response.Content = new StreamContent(stream);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue(GetMimeType(@"www\index.html"));
+                return response;
+            }
+            catch
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+        }
+
+        private static string GetMimeType(string filePath)
+        {
+            var fileInfo = new FileInfo(filePath);
+            return MimeTypesMap.GetMimeType(fileInfo.Extension);
+        }
+    }
+}
+
